4be4c4c029e50922ceca77e476b53ff97eff2280
Karol Galanciak <karol.galanciak@gmail.com>
2021-03-10 09:34:27 +0100

    fix the require statements

--- lib/tartarus/remote_storage/glacier.rb
+++ lib/tartarus/remote_storage/glacier.rb
@@ -1,4 +1,9 @@
 require "aws-sdk-glacier"
+require "tartarus/remote_storage/glacier/client"
+require "tartarus/remote_storage/glacier/file"
+require "tartarus/remote_storage/glacier/csv_export"
+require "tartarus/remote_storage/glacier/register_upload"
+require "tartarus/remote_storage/glacier/configuration"
 
 class Tartarus
   module RemoteStorage
